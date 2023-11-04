f87539d1dcbd0aae8ab5ff921a0c64db1431a0f9
Abhishek28112002 <pawanmittal2002@gmail.com>
2023-11-04 22:03:27 +0530

    .

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 from fastapi.middleware.cors import CORSMiddleware
-
+from motor.motor_asyncio import AsyncIOMotorClient
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
@@ -10,6 +10,17 @@ app.add_middleware(CORSMiddleware,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],)
+
+
+# MongoDB connection URL
+mongo_uri = "mongodb+srv://pawanmittal2002:abhi2811@cluster0.9vjktto.mongodb.net/"
+
+# Create a MongoDB client
+client = AsyncIOMotorClient(mongo_uri)
+
+# Access a specific MongoDB database
+database = client["IIT_DH_OLX"]
+
 
 @app.get("/")
 async def root():
