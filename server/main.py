b7d2f379e937b46989ac8903a7b6cac3cda2105c
Shriagrawal <shristiagrawal04@gmail.com>
2023-11-05 06:28:47 +0530

    lk

--- server/main.py
+++ server/main.py
@@ -1,12 +1,17 @@
 import uvicorn
-from fastapi import FastAPI
-from fastapi_sqlalchemy import DBSessionMiddleware, db
+from fastapi import FastAPI,HTTPException
+# from fastapi_sqlalchemy import DBSessionMiddleware, db
 from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import FastAPI,Depends
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session,sessionmaker,SessionLocal
-from Models import User  # Replace with your actual module and User model import
+# from sqlalchemy import create_engine, select
+# from sqlalchemy.orm import session,sessionmaker
+# from Models import User
+from pymongo import MongoClient
+
+# from Models import User  # Replace with your actual module and User model import
+
+
 
 app = FastAPI()
 
@@ -20,28 +25,30 @@ app.add_middleware(CORSMiddleware,
 mongo_uri = "mongodb+srv://pawanmittal2002:abhi2811@cluster0.9vjktto.mongodb.net/"
 
 # Create a MongoDB client
-client = AsyncIOMotorClient(mongo_uri)
+client = MongoClient(mongo_uri)
 
 # Access a specific MongoDB database
 database = client["IIT_DH_OLX"]
+# collection_name = ""
+
+# if collection_name not in database.list_collection_names():
+#     database.create_collection(collection_name)
 
 
 @app.get("/")
 async def root():
-    
     return {"message": "Hello World"}
 
 @app.get("/users/")
-def get_users(db: Session = Depends(User)):
-    users = db.query(User).all()
+def get_users():
+    collection = database['users']
+    users = collection.find()
     return users
-
 
 @app.get("/users/{username}")
 def get_user(username: str):
-    db = SessionLocal()
-    user = db.execute(select(User).where(User.username == username)).scalar()
-    db.close()
+    collection = database['users']
+    user = collection.find_one({"username": username})
 
     if user:
         return {"user_id": user.id, "username": user.username, "email": user.email}
@@ -49,11 +56,73 @@ def get_user(username: str):
         return {"message": "User not found"}
 
 
+@app.post("/add_user")
+async def add_user(new_user : dict):
+    print(new_user)
+    users_dict = new_user
+    collection = database.get_collection('users')
+    if new_user._id:
+        return {"User aldready exists"}
+    else:
+        result =  collection.insert_one(users_dict)
+    if result:
+        return {"UPDATED SUCESSFULLY"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to insert item into the database")
+    
+    
+@app.post("/add_item")
+def add_item(new_item : dict):
+    products_dict = new_item
+    collection = database['products']
+    result = collection.insert_one(products_dict)
+    if(result):
+        return {"Data added successfully"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to insert item into the database")
+    
+    
+@app.post("/add_merchandise")
+def add_merchandise(new_item : dict):
+    new_dict = new_item
+    collection = database['merchandise']
+    result = collection.insert_one(new_dict)
+    if result:
+        return {"SUCCESSFULLY ADDED"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to insert item into the database")
 
-def hash_password(password: str):
-    from passlib.context import CryptContext
-    password_helper = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
 
+# @app.post("/donations")
+# def add_donations(item : dict):
+#     new_dict = item
+#     collection = database['donations']
+#     result = collection.insert_one(new_dict)
+#     if result:
+#         return {"SUCCESSFULLY ADDED"}
+#     else:
+#         raise HTTPException(status_code=500, detail="Failed to insert item into the database")
+
+@app.post("/add_alumni")
+def add_alumni(item : dict):
+    new_dict = item
+    collection = database['alumni']
+    result = collection.insert_one(new_dict)
+    if result:
+        return {"SUCCESSFULLY ADDED"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to insert item into the database")
+    
+
+@app.post("/signup")
+def signup(user : dict):
+    new_user = user
+    collection = database['users']
+    result = collection.insert_one(new_user)
+    if result:
+        return {"SUCCESSFULLY ADDED"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to insert item into the database")
 
 if __name__ == "__main__":
     import uvicorn
